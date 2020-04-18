3c945202cd76b7e936084aed76212c6399968e5e
QiaoKeliHenku <2697183457@qq.com>
2020-04-18 14:35:55 +0800

    完成路由懒加载

--- react-hook-music/src/App.js
+++ react-hook-music/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 // 引入对应的路由内置的组件
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-// 引入对应的路径组件
-import Home from './views/Home/Home'
+// 引入对应的css样式
 import './style/app.scss';
 
+// 路由懒加载
+const Home = lazy(() => import('./views/Home/Home'));
 function App() {
   return (
     <BrowserRouter>
-      {/*  一级路由*/}
-      <Switch>
-        <Route to={"/home"} component={Home}/>
-      </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+            {/*  一级路由*/}
+            <Switch>
+                <Route to={"/home"} component={Home}/>
+            </Switch>
+        </Suspense>
     </BrowserRouter>
   );
 }
--- react-hook-music/src/views/Home/Home.js
+++ react-hook-music/src/views/Home/Home.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 // 引入对应的路由内置组件
 import {Switch, Route} from 'react-router-dom'
-// 引入对应的路由组件
-import Recommend from '../Recommend/Recommend'
-import HotSong from "../HotSong/HotSong";
-import Search from "../Search/Search";
+
 // 引入对应的css文件
 import '../../style/home.scss'
 // 引入当前组件下面的子组件
 import HomeTopHeader from './base/TopHeader'
 import HomeNavBar from './base/NavBar'
+// 路由懒加载
+const Recommend = lazy(() => import('../Recommend/Recommend'))
+const HotSong = lazy(() => import('../HotSong/HotSong'))
+const Search = lazy(() => import('../Search/Search'))
 function Home () {
     return (
         <div className={"home"}>
@@ -17,12 +18,14 @@ function Home () {
                 <HomeTopHeader />
                 <HomeNavBar />
             </div>
-            {/*二级路由*/}
-            <Switch>
-                <Route path={"/home/recommend"} component={Recommend}/>
-                <Route path={"/home/hotsong"} component={HotSong}/>
-                <Route path={"/home/search"} component={Search}/>
-            </Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+                {/*二级路由*/}
+                <Switch>
+                    <Route path={"/home/recommend"} component={Recommend}/>
+                    <Route path={"/home/hotsong"} component={HotSong}/>
+                    <Route path={"/home/search"} component={Search}/>
+                </Switch>
+            </Suspense>
         </div>
     )
 }
