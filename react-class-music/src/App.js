90e422b45323d7e79b933271d442eb42ee2e7f72
巧克力很苦 <7633857+qklhk@user.noreply.gitee.com>
2020-08-02 14:23:39 +0800

    初始化项目

---
+++ react-class-music/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hello ReactCli
+      </div>
+    );
+  }
+}
+
+export default App;
